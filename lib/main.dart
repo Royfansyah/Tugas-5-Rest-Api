9af34615c21a92d49d459ccfad0a524eaefbdcd0
Roy <royfansyahae@gmail.com>
2023-11-13 20:07:56 +0700

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'details.dart';
+import 'newdata.dart';
+
+void main() => runApp(MaterialApp(
+      title: "Api Test",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: const Home(),
+    ));
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  Future<List> getData() async {
+    var url = Uri.parse('http://192.168.18.5/restapi2/list.php'); //Api Link
+    final response = await http.post(url);
+    return jsonDecode(response.body);
+  }
+
+  @override
+  void initState() {
+    getData();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text("Data Karyawan"),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (BuildContext contex) => NewData(),
+          ),
+        ),
+        child: const Icon(Icons.add),
+      ),
+      body: FutureBuilder<List>(
+        future: getData(),
+        builder: (ctx, ss) {
+          if (ss.hasError) {
+            print("error");
+          }
+          if (ss.hasData) {
+            return Items(list: ss.data!);
+          } else {
+            return const CircularProgressIndicator();
+          }
+        },
+      ),
+    );
+  }
+}
+
+class Items extends StatelessWidget {
+  List list;
+  Items({Key? key, required this.list}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: list == null ? 0 : list.length,
+        itemBuilder: (ctx, i) {
+          return ListTile(
+            leading: const Icon(Icons.text_snippet_outlined),
+            title: Text(list[i]['nama']), //Key
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(list[i]['alamat']), //Key
+                Text(list[i]['salary']), //Key
+              ],
+            ),
+            trailing: ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (BuildContext context) =>
+                      Details(list: list, index: i),
+                ));
+              },
+              child: Text("Details"),
+            ),
+          );
+        });
+  }
+}
